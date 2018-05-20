2dc2c4bd5ffd950b7117b330306b7362cdc0d98a
matttattoli <matttattoli@gmail.com>
2018-05-20 17:54:35 -0400

    new parse option

--- cogs/Reminders.py
+++ cogs/Reminders.py
@@ -10,21 +10,23 @@ import asyncio
 
 class RemindParser:
     def parseReminderMsg(msg: str):
+        timedelta = ''
         if msg.count(" to ") > 0 and msg.count("in ") > 0:
             data = msg.split(" to ")
             timedelta = data[0].split("in ")[1]
             remindmsg = data[1]
-            if timedelta.endswith("s"):
-                timedelta = datetime.datetime.now() + datetime.timedelta(seconds=int(timedelta[:-1]))
-            elif timedelta.endswith("m"):
-                timedelta = datetime.datetime.now() + datetime.timedelta(minutes=int(timedelta[:-1]))
-            elif timedelta.endswith("h"):
-                timedelta = datetime.datetime.now() + datetime.timedelta(hours=int(timedelta[:-1]))
-            elif timedelta.endswith("d"):
-                timedelta = datetime.datetime.now() + datetime.timedelta(days=int(timedelta[:-1]))
-            return timedelta, remindmsg
-        else:
-            return None
+        if timedelta == '':
+            timedelta = msg.split(" ")[0]
+            remindmsg = msg.replace(timedelta + " ", "")
+        if timedelta.endswith("s"):
+            timedelta = datetime.datetime.now() + datetime.timedelta(seconds=int(timedelta[:-1]))
+        elif timedelta.endswith("m"):
+            timedelta = datetime.datetime.now() + datetime.timedelta(minutes=int(timedelta[:-1]))
+        elif timedelta.endswith("h"):
+            timedelta = datetime.datetime.now() + datetime.timedelta(hours=int(timedelta[:-1]))
+        elif timedelta.endswith("d"):
+            timedelta = datetime.datetime.now() + datetime.timedelta(days=int(timedelta[:-1]))
+        return timedelta, remindmsg
 
 
 class Reminders:
@@ -59,7 +61,7 @@ class Reminders:
             if len(expiredRems) >= 1:
                 for rem in expiredRems:
                     await self.get_channel(rem[1]).\
-                        send(f"{self.get_user(rem[0])}, you asked to be reminded to {rem[4]}")
+                        send(f"{self.get_user(rem[0]).mention}, you asked to be reminded to {rem[4]}")
                     Database.deleteReminder(rem[0], rem[2], rem[3], rem[4])
             await asyncio.sleep(10)
 
