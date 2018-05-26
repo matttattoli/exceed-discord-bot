520918e1a046d46f448c832f5c581dbb65232b3d
matttattoli <matttattoli@gmail.com>
2018-05-26 15:45:43 -0400

    converting sql library from sqlite3 to asyncpg, using postgre as host

--- cogs/utils/Database.py
+++ cogs/utils/Database.py
@@ -1,104 +1,93 @@
 import discord
 from discord.ext import commands
-import sqlite3
+import asyncpg
 import datetime
+from config import *
 
 
-db = sqlite3.connect("exceeddatabase.db")
-cursor = db.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS `guilds` (
-        `gid`	INTEGER NOT NULL UNIQUE,
-        `log_mode`	INTEGER DEFAULT 0,
-        `log_channel`	INTEGER DEFAULT NULL,
-        `bot_volume`    INTEGER REAL DEFAULT 0.5,
-        PRIMARY KEY(`gid`)
-        )""")
-cursor.execute("""CREATE TABLE IF NOT EXISTS `reminders` (
-        `userid` INTEGER NOT NULL,
-        `channelid`	INTEGER NOT NULL,
-        `created_at`	TEXT NOT NULL,
-        `expires`	TEXT NOT NULL,
-        `message` TEXT DEFAULT NULL
-        )""")
-cursor.execute("""CREATE TABLE IF NOT EXISTS `funroles` (
-        `gid` INTEGER NOT NULL,
-        `rolename` TEXT NOT NULL,
-        `roleid` INTEGER NOT NULL UNIQUE,
-        PRIMARY KEY(`roleid`)
-        )""")
-db.commit()
+async def __init__():
+    global db
+    db = await asyncpg.connect(user=privateconfig["database"]["user"], password=privateconfig["database"]["password"],
+                               database=privateconfig["database"]["database"], host=privateconfig["database"]["host"])
+    await db.execute("""CREATE TABLE IF NOT EXISTS guilds (
+            gid	INTEGER NOT NULL UNIQUE,
+            log_mode	INTEGER DEFAULT 0,
+            log_channel	INTEGER DEFAULT NULL,
+            bot_volume    INTEGER DEFAULT 0.5,
+            PRIMARY KEY(gid)
+            )""")
+    await db.execute("""CREATE TABLE IF NOT EXISTS reminders (
+            userid INTEGER NOT NULL,
+            channelid	INTEGER NOT NULL,
+            created_at	TIMESTAMP NOT NULL,
+            expires	TIMESTAMP NOT NULL,
+            message TEXT DEFAULT NULL
+            )""")
+    await db.execute("""CREATE TABLE IF NOT EXISTS funroles (
+            gid INTEGER NOT NULL,
+            rolename TEXT NOT NULL,
+            roleid INTEGER NOT NULL UNIQUE,
+            PRIMARY KEY(roleid)
+            )""")
+    print("INITIALIZED DB")
+# TODO: convert all sqlite3 to postgre/asyncpg
+# TODO: convert all queries into f strings with proper format for postgre : DONE
 
 
 class Database:
-    def initializeGuild(gid: int):
-        cursor.execute("SELECT gid FROM guilds WHERE gid = ?", (gid,))
-        data = cursor.fetchone()
+    async def initializeGuild(gid: int):
+        data = await db.fetch(f"SELECT gid FROM guilds WHERE gid = {gid}")
         if data is None:
-            cursor.execute("INSERT INTO guilds(gid) VALUES(?)", (gid,))
-        db.commit()
+            db.execute(f"INSERT INTO guilds(gid) VALUES({gid})")
 
-    def setLogChannel(gid: int, logmode: int, logchan: int):
-        cursor.execute("SELECT gid FROM guilds WHERE gid = ?", (gid,))
-        data = cursor.fetchone()
+    async def setLogChannel(gid: int, logmode: int, logchan: int):
+        data = await db.fetch(f"SELECT gid FROM guilds WHERE gid = {gid}")
         if data is None:
-            cursor.execute("""INSERT INTO guilds(gid, log_mode, log_channel) VALUES(?,?,?)
-                            """, (gid, logmode, logchan))
+            await db.execute(f"""INSERT INTO guilds(gid, log_mode, log_channel) VALUES({gid},{logmode},{logchan})""")
         else:
-            cursor.execute("""UPDATE guilds SET log_mode=?, log_channel=? WHERE gid = ?""",
-                           (logmode, logchan, gid))
-        db.commit()
+            await db.execute(f"""UPDATE guilds SET log_mode={logmode}, log_channel={logchan} WHERE gid = {gid}""")
 
-    def getLogChannel(gid: int):
-        cursor.execute("SELECT `log_mode`, `log_channel` from guilds WHERE gid = ?", (gid,))
-        data = cursor.fetchone()
+    async def getLogChannel(gid: int):
+        data = await db.fetch(f"SELECT log_mode, log_channel from guilds WHERE gid = {gid}")
         if data is None or data[0] == 0:
             return None
         else:
             return data[1]
 
-    def addfunrole(gid: int, role: discord.Role):
-        cursor.execute("SELECT gid FROM funroles WHERE roleid = ?", (role.id,))
-        data = cursor.fetchone()
+    async def addfunrole(gid: int, role: discord.Role):
+        data = await db.fetch(f"SELECT gid FROM funroles WHERE roleid = {role.id}")
         if data is None:
-            cursor.execute("""INSERT INTO funroles(gid, rolename, roleid) VALUES(?,?,?)""", (gid, role.name, role.id))
+            await db.execute(f"""INSERT INTO funroles(gid, rolename, roleid) VALUES({gid},'{role.name}',{role.id})""")
         else:
             return
-        db.commit()
 
-    def removefunrole(gid: int, role: discord.Role):
-        cursor.execute("DELETE FROM funroles WHERE roleid = ?", (role.id,))
-        db.commit()
+    async def removefunrole(gid: int, role: discord.Role):
+        await db.execute(f"DELETE FROM funroles WHERE roleid = {role.id}")
 
-    def isfunrole(gid: int, roleid: int):
-        cursor.execute("SELECT roleid, rolename FROM funroles WHERE roleid = ?", (roleid,))
-        data = cursor.fetchone()
+    async def isfunrole(gid: int, roleid: int):
+        data = await db.fetch(f"SELECT roleid, rolename FROM funroles WHERE roleid = {roleid}")  # LIMIT 1 ??
         if data is not None:
             return True
         else:
             return False
 
-    def getFunRoles(gid: int):
-        cursor.execute("SELECT rolename FROM funroles WHERE gid = ?", (gid,))
-        data = cursor.fetchall()
+    async def getFunRoles(gid: int):
+        data = await db.fetch(f"SELECT rolename FROM funroles WHERE gid = {gid}")
         return data
 
-    def getReminders(userid: int):
-        cursor.execute("SELECT * FROM reminders WHERE userid = ?", (userid,))
-        data = cursor.fetchall()
+    async def getReminders(userid: int):
+        data = await db.fetch(f"SELECT * FROM reminders WHERE userid = {userid}")
         return data
 
-    def createReminder(userid: int, channelid: int, created_at: datetime.datetime,
+    async def createReminder(userid: int, channelid: int, created_at: datetime.datetime,
                        expires: datetime.datetime, message: str=None):
-        cursor.execute("INSERT INTO reminders(userid, channelid, created_at, expires, message) VALUES(?,?,?,?,?)", (
-            userid, channelid, created_at, expires, message,))
-        db.commit()
+        db.execute(f"""INSERT INTO reminders(userid, channelid, created_at, expires, message) 
+        VALUES({userid},{channelid},TIMESTAMP '{created_at}',TIMESTAMP '{expires}','{message}')""")
 
-    def getExpired():
-        cursor.execute("SELECT * FROM reminders WHERE expires < ?", (datetime.datetime.now(),))
-        data = cursor.fetchall()
+    async def getExpired():
+        data = db.fetch(f"SELECT * FROM reminders WHERE expires < {datetime.datetime.now()}")
         return data
 
-    def deleteReminder(userid: int, created_at: datetime.datetime, expires: datetime.datetime, message: str=None):
-        cursor.execute("DELETE FROM reminders WHERE userid = ? AND created_at = ? AND expires = ? AND message = ?",
-                       (userid, created_at, expires, message,))
-        db.commit()
+    async def deleteReminder(userid: int, created_at: datetime.datetime, expires: datetime.datetime, message: str=None):
+        await db.execute(f"""DELETE FROM reminders WHERE userid = {userid} AND created_at = {created_at}
+                         AND expires = {expires} AND message = {message}""")
