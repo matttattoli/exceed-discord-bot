74fe6e826ea257197febe1ac028abb66c92485a5
matttattoli <matttattoli@gmail.com>
2019-01-19 23:43:56 -0500

    cleaned up config module

--- config.py
+++ config.py
@@ -6,9 +6,3 @@ privateconfig_file = path.join(path.dirname(__file__), 'privateconfig.json')
 
 config = json.load(open(config_file, 'r'))
 privateconfig = json.load(open(privateconfig_file, 'r'))
-#  TODO: add removeaccess database. add live database
-"""
-def reloadconfigs():
-    config = json.load(open(config_file, 'r'))
-    privateconfig = json.load(open(privateconfig_file, 'r'))
-"""
